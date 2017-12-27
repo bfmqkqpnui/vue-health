858b1075d21f26e591ab89a823a12014d78ca614
lance <47877962@qq.com>
2017-12-28 07:31:54 +0800

    health By Dormitory 2017-12-28 07:30

---
+++ src/components/head/head.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div id="u30926" class="ax_default box_1">
+      <div id="u30926_div" class=""></div>
+      <!-- Unnamed () -->
+      <div id="u30927" class="text" style="display:none; visibility: hidden">
+        <p><span></span></p>
+      </div>
+    </div>
+
+    <div id="u30928" class="ax_default label">
+      <div id="u30928_div" class=""></div>
+      <!-- Unnamed () -->
+      <div id="u30929" class="text">
+        <p><span>屿真健康科技</span></p>
+      </div>
+    </div>
+
+    <div id="u30930" class="ax_default box_2" data-label="系统" style="cursor: pointer;">
+      <div id="u30930_div" class="" tabindex="0"></div>
+      <!-- Unnamed () -->
+      <div id="u30931" class="text" style="top: 19px; transform-origin: 63px 9.5px 0px;">
+        <p id="cache10" style=""><span id="cache11" style="">系统</span></p>
+      </div>
+    </div>
+
+    <div id="u30932" class="ax_default box_2 selected" data-label="门店" style="cursor: pointer;">
+      <div id="u30932_div" class="selected" tabindex="0"></div>
+      <!-- Unnamed () -->
+      <div id="u30933" class="text" style="top: 19px; transform-origin: 63px 9.5px 0px;">
+        <p id="cache0"><span id="cache1">门店</span></p>
+      </div>
+    </div>
+
+    <div id="u30934" class="ax_default box_2" data-label="诊所" style="cursor: pointer;">
+      <div id="u30934_div" class="" tabindex="0"></div>
+      <!-- Unnamed () -->
+      <div id="u30935" class="text" style="top: 19px; transform-origin: 63px 9.5px 0px;">
+        <p><span>诊所</span></p>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    computed: {},
+    methods: {
+      setPageTitle: function () {
+        document.body.style.background = '';
+        document.title = '首页-订单派送';
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ static/css/销售排行/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p],q,_(r,s,t,u,v,w,x,_(),y,_(z,A,B,C,D,_(E,F,G,H),I,null,J,K,L,K,M,N,O,null,P,Q,R,S,T,U,V,Q),W,_(),X,_(),Y,_(Z,[])),ba,_(),bb,_());}; 
+var b="url",c="销售排行.html",d="generationDate",e=new Date(1513937048743.94),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="menu",o="side_bar",p="system",q="page",r="packageId",s="705af587d27a4a02a947a62737b0c380",t="type",u="Axure:Page",v="name",w="销售排行",x="notes",y="style",z="baseStyle",A="627587b6038d43cca051c114ac41ad32",B="pageAlignment",C="center",D="fill",E="fillType",F="solid",G="color",H=0xFFFFFFFF,I="image",J="imageHorizontalAlignment",K="near",L="imageVerticalAlignment",M="imageRepeat",N="auto",O="favicon",P="sketchFactor",Q="0",R="colorStyle",S="appliedColor",T="fontName",U="Applied Font",V="borderWidth",W="adaptiveStyles",X="interactionMap",Y="diagram",Z="objects",ba="masters",bb="objectPaths";
+return _creator();
+})());
