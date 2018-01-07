225a99067db0a7518ddc8ccdd678919e155bcddf
bfmqkqpnui <47877962@qq.com>
2018-01-07 18:49:47 +0800

    vue-health By Home 2018-01-07 18:49

---
+++ src/components/main/member/rechargeRecord.vue
@@ -0,0 +1,30 @@
+<!-- vue模板 -->
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  //Js部分尽量采用ES6语法，webpack babel插件会转义兼容
+  export default {
+    //组件私有数据（必须是function，而且要return对象类型）
+    data() {
+      return {}
+    },
+    //计算属性
+    computed: {},
+    //函数集，自己封装，便于开发使用
+    methods: {},
+    //生命周期钩子：组件实例渲染完成时调用
+    mounted() {
+
+    },
+    //要用到哪些子组件（如果组件已是最小粒度，那么可省略该属性）
+    components: {}
+  }
+</script>
+
+<style>
+
+</style>
